33c47d2d9eaa5d6feae9b06c8d819da0bde4cc1c
Mateusz Śliwakowski <01130631@pw.edu.pl>
2019-10-16 22:52:42 +0200

    Add mock static joboffer list

--- hr-application/Models/JobOffer.cs
+++ hr-application/Models/JobOffer.cs
@@ -8,6 +8,13 @@ namespace hr_application.Models
 {
     public class JobOffer
     {
+        public static List<JobOffer> _jobOffers = new List<JobOffer>
+        {
+            new JobOffer {Id = 0, JobTitle = "Developer", Description = ".NET Core MVC programmer.", MinimumSalary = 3000, MaximumSalary = 5000, ExpirationDate = new DateTime(2019,12,30), Location = "Warsaw"},
+            new JobOffer {Id = 0, JobTitle = "Frontend Developer", Description = "Java Script programmer.", MinimumSalary = 3000, MaximumSalary = 5000, ExpirationDate = new DateTime(2019,12,30), Location = "Warsaw"},
+            new JobOffer {Id = 0, JobTitle = "Manager", Description = "Lead of big team", MinimumSalary = 3000, MaximumSalary = 5000, ExpirationDate = new DateTime(2019,12,30), Location = "Warsaw"}
+        };
+
         public int Id { get; set; }
 
         [Required]
