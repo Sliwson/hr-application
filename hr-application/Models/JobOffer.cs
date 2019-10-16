dfb69c2f1ff9812979f672381c5f7b02e12e9004
Mateusz Śliwakowski <01130631@pw.edu.pl>
2019-10-16 22:40:47 +0200

    Add JobOffer model with specified fields

---
+++ hr-application/Models/JobOffer.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace hr_application.Models
+{
+    public class JobOffer
+    {
+        public int Id { get; set; }
+        public string JobTitle { get; set; }
+        public string Description { get; set; }
+        public int MinimumSalary { get; set; }
+        public int MaximumSalary { get; set; }
+        public string Location { get; set; }
+
+        [DataType(DataType.Date)]
+        public DateTime ExpirationDate { get; set }
+
+    }
+}
