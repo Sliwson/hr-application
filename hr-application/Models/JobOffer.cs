09d5b4ae73303504760cddc57002440188856f50
Mateusz Śliwakowski <01130631@pw.edu.pl>
2019-10-16 22:46:57 +0200

    Add basic validation

--- hr-application/Models/JobOffer.cs
+++ hr-application/Models/JobOffer.cs
@@ -9,14 +9,30 @@ namespace hr_application.Models
     public class JobOffer
     {
         public int Id { get; set; }
+
+        [Required]
+        [StringLength(60, MinimumLength = 5)]
         public string JobTitle { get; set; }
+
+        [Required]
+        [StringLength(1000, MinimumLength = 5)]
         public string Description { get; set; }
+
+        [Range(0, 1000000)]
+        [DataType(DataType.Currency)]
         public int MinimumSalary { get; set; }
+
+        [Range(0, 1000000)]
+        [DataType(DataType.Currency)]
         public int MaximumSalary { get; set; }
+
+        [Required]
+        [StringLength(100, MinimumLength = 5)]
         public string Location { get; set; }
 
+        [Required]
         [DataType(DataType.Date)]
-        public DateTime ExpirationDate { get; set }
+        public DateTime ExpirationDate { get; set; }
 
     }
 }
