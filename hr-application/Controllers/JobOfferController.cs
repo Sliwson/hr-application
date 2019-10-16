9c0d3d320f9c32cdcb4748eeee5e3c3ad3360bc8
Mateusz Śliwakowski <01130631@pw.edu.pl>
2019-10-16 23:12:45 +0200

    Add autogenerated view, showing JobOffer model

--- hr-application/Controllers/JobOfferController.cs
+++ hr-application/Controllers/JobOfferController.cs
@@ -3,14 +3,15 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using hr_application.Models;
 
 namespace hr_application.Controllers
 {
     public class JobOfferController : Controller
     {
-        public IActionResult Index()
+        public IActionResult List()
         {
-            return View();
+            return View(JobOffer._jobOffers);
         }
     }
 }
