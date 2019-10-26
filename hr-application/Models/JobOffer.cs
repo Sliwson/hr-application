2847036f89b2e38c2433041363a5488c747f5bf0
Mateusz Śliwakowski <01130631@pw.edu.pl>
2019-10-26 13:21:31 +0200

    Improve JobOffer model validation #37

--- hr-application/Models/JobOffer.cs
+++ hr-application/Models/JobOffer.cs
@@ -6,7 +6,7 @@ using System.ComponentModel.DataAnnotations;
 
 namespace hr_application.Models
 {
-    public class JobOffer
+    public class JobOffer : IValidatableObject
     {
         public static List<JobOffer> _jobOffers = new List<JobOffer>
         {
@@ -18,11 +18,11 @@ namespace hr_application.Models
         public int Id { get; set; }
 
         [Required]
-        [StringLength(60, MinimumLength = 5)]
+        [StringLength(60)]
         public string JobTitle { get; set; }
 
         [Required]
-        [StringLength(1000, MinimumLength = 5)]
+        [StringLength(1000)]
         public string Description { get; set; }
 
         [Range(0, 1000000)]
@@ -34,12 +34,20 @@ namespace hr_application.Models
         public int MaximumSalary { get; set; }
 
         [Required]
-        [StringLength(100, MinimumLength = 5)]
+        [StringLength(100)]
         public string Location { get; set; }
 
         [Required]
         [DataType(DataType.Date)]
         public DateTime ExpirationDate { get; set; }
 
+        public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+        {
+            if (ExpirationDate <= DateTime.Now)
+                yield return new ValidationResult("Expiration date must be greater than current date");
+
+            if (MaximumSalary < MinimumSalary)
+                yield return new ValidationResult("Maximum Salary must be greater or equal to Minimum Salary");
+        }
     }
 }
