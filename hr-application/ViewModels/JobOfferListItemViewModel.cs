c53f0b0ab7e832e37e139296663040dd29efa87e
Mateusz Śliwakowski <01130631@pw.edu.pl>
2019-10-26 16:26:23 +0200

    Add ViewModel for JobOffer list #77

---
+++ hr-application/ViewModels/JobOfferListItemViewModel.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+using hr_application.Models;
+
+namespace hr_application.ViewModels
+{
+    public class JobOfferListItemViewModel
+    {
+        public JobOfferListItemViewModel(string jobTitle, string salaryRange, string location, DateTime expirationDate)
+        {
+            JobTitle = jobTitle;
+            SalaryRange = salaryRange;
+            Location = location;
+            ExpirationDate = expirationDate;
+        }
+
+        public string JobTitle { get; set; }
+
+        public string SalaryRange { get; set; }
+
+        public string Location { get; set; }
+
+        [DataType(DataType.Date)]
+        public DateTime ExpirationDate { get; set; }
+    }
+}
