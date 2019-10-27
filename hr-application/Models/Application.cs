7f4c2dcb79a9489574e8d86fd395ef5c079fc4a2
Mateusz Śliwakowski <01130631@pw.edu.pl>
2019-10-27 10:47:41 +0100

    Add simple Application model #45

---
+++ hr-application/Models/Application.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace hr_application.Models
+{
+    public class Application
+    {
+        public int Id { get; set; }
+
+        public string Email { get; set; }
+
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public string PhoneNumber { get; set; }
+
+        public IFormFile CoverLetterFile { get; set; }
+
+        public IFormFile CVFile { get; set; }
+    }
+}
