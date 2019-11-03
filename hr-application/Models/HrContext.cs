1a5f7d7a03eb2f7618ed3e6508ec5f2e0c51cdd0
Mateusz Śliwakowski <mateusz_sliw@interia.pl>
2019-11-03 11:41:47 +0100

    Add HrContext with local connection string #60

---
+++ hr-application/Models/HrContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
+using hr_application.Models;
+
+namespace hr_application
+{
+    public class HrContext : DbContext
+    {
+        public HrContext(DbContextOptions<HrContext> options)
+        : base(options) { }
+
+        public DbSet<JobOffer> JobOffers { get; set; }
+        public DbSet<Application> Applications { get; set; }
+    }
+}
--- hr-application/Startup.cs
+++ hr-application/Startup.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
@@ -31,6 +32,9 @@ namespace hr_application
                 options.MinimumSameSitePolicy = SameSiteMode.None;
             });
 
+            services.AddDbContext<HrContext>(options => {
+                options.UseSqlServer(Configuration.GetConnectionString("LocalDatabase"));
+            });
 
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
         }
