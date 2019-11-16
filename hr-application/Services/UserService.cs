82218f0657cc6ab6deb8c41c7f39f48903cbf495
Mateusz Śliwakowski <mateusz_sliw@interia.pl>
2019-11-16 16:19:44 +0100

    Add IUserService with simple mock implementation #86

---
+++ hr-application/Services/UserService.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace hr_application.Services
+{
+    public enum UserRole
+    {
+        NoAuth,
+        User,
+        Hr,
+        Admin
+    }
+
+    public interface IUserService
+    {
+        bool IsAuthenticated();
+        string GetUserId();
+        UserRole GetUserRole();
+        string GetRedirectToLoginUrl();
+    }
+
+    public class MockUserService : IUserService
+    {
+        public string GetUserId()
+        {
+            return "test-user";
+        }
+
+        public UserRole GetUserRole()
+        {
+            return UserRole.Admin;
+        }
+
+        public bool IsAuthenticated()
+        {
+            return true;
+        }
+
+        public string GetRedirectToLoginUrl()
+        {
+            return "Index";
+        }
+    }
+}
