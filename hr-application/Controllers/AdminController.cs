f65bbdc4a77eae41ff0aabd98efe478745898630
Mateusz Śliwakowski <mateusz_sliw@interia.pl>
2019-11-16 16:37:25 +0100

    Add simple admin controller and user model #55

---
+++ hr-application/Controllers/AdminController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using hr_application.Services;
+
+namespace hr_application.Controllers
+{
+    public class AdminController : Controller
+    {
+        private IUserService userService;
+
+        public AdminController(IUserService userService)
+        {
+            this.userService = userService;
+        }
+
+        public IActionResult Index()
+        {
+            if (userService.GetUserRole() != UserRole.Admin)
+                return Forbid();
+
+            return View();
+        }
+    }
+}
---
+++ hr-application/Models/User.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using hr_application.Services;
+
+namespace hr_application.Models
+{
+    public class User
+    {
+        public string Identifier { get; set; }
+        public  UserRole Role { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+    }
+}
