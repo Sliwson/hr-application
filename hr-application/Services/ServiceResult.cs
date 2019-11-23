4cae884aec8adcb87ba70ce17babaa7c5c72559c
Mateusz Śliwakowski <mateusz_sliw@interia.pl>
2019-11-23 20:12:07 +0100

    Add service result enum for cleanup #101

---
+++ hr-application/Services/ServiceResult.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace hr_application.Services
+{
+    public enum ServiceResult
+    {
+        OK,
+        NotAuthorized,
+        NotFound
+    }
+}
