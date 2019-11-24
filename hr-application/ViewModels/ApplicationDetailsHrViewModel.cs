532e87a8b4484e7d7b25286aee0d986279c9fede
Mateusz Śliwakowski <01130631@pw.edu.pl>
2019-11-24 23:03:45 +0100

    Add application details view model for hr #63

---
+++ hr-application/ViewModels/ApplicationDetailsHrViewModel.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using hr_application.Models;
+
+namespace hr_application.ViewModels
+{
+    public class ApplicationDetailsHrViewModel
+    {
+        public ApplicationDetailsHrViewModel(Application application)
+        {
+            Id = application.Id;
+            Email = application.Email;
+            FirstName = application.FirstName;
+            LastName = application.LastName;
+            PhoneNumber = application.PhoneNumber;
+            State = application.State;
+
+        }
+
+        public Guid Id { get; set; }
+        public string Email { get; set; }
+
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public string PhoneNumber { get; set; }
+
+        public ApplicationState State { get; set; }
+    }
+}
