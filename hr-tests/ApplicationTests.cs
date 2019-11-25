0fd03ab26056a9f7e917b824ec3830d92f3b832c
Mateusz Śliwakowski <01130631@pw.edu.pl>
2019-11-25 22:06:42 +0100

    Add test project to solution #74

---
+++ hr-tests/ApplicationTests.cs
@@ -0,0 +1,21 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace hr_tests
+{
+    class ApplicationTests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
