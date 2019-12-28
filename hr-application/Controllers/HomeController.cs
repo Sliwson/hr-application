0eadad2409efce0fcc67350e71e91e0bd97e9821
Mateusz Śliwakowski <01130631@pw.edu.pl>
2019-12-28 14:05:30 +0100

    Update role debug features #120

--- hr-application/Controllers/HomeController.cs
+++ hr-application/Controllers/HomeController.cs
@@ -5,13 +5,21 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using hr_application.Models;
+using hr_application.Services;
 
 namespace hr_application.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly IUserService userService;
+
+        public HomeController(IUserService userService)
+        {
+            this.userService = userService;
+        }
         public IActionResult Index()
         {
+            ViewData["Role"] = userService.GetUserRole();
             return View();
         }
 
@@ -25,5 +33,19 @@ namespace hr_application.Controllers
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
+
+#if DEBUG
+        public IActionResult Login()
+        {
+            userService.AuthenticateAs(UserRole.User);
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult Logout()
+        {
+            userService.AuthenticateAs(UserRole.NoAuth);
+            return RedirectToAction("Index");
+        }
+#endif
     }
 }
