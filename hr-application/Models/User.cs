2dc4364266c2649c293c0bf59dec31db84ee3aed
Mateusz Śliwakowski <01130631@pw.edu.pl>
2019-12-29 20:03:00 +0100

    Add email field to user model #124

--- hr-application/Models/User.cs
+++ hr-application/Models/User.cs
@@ -12,5 +12,6 @@ namespace hr_application.Models
         public  UserRole Role { get; set; }
         public string Name { get; set; }
         public string Surname { get; set; }
+        public string Email { get; set; }
     }
 }
