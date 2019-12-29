d38e9fb38e55e2c6b586e53eb37c10bfc26c331f
Mateusz Śliwakowski <01130631@pw.edu.pl>
2019-12-29 20:01:24 +0100

    Add debug user service implementation using adb2c #124

---
+++ hr-application/Services/AdUserService.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Web;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+
+namespace hr_application.Services
+{
+    public class AdUserService : IUserService
+    {
+        private IHttpContextAccessor context;
+        private static UserRole tmpRole = UserRole.User;
+
+        public AdUserService(IHttpContextAccessor context)
+        {
+            this.context = context;
+        }
+
+        public void AuthenticateAs(UserRole role)
+        {
+            tmpRole = role;
+        }
+
+        public string GetRedirectToLoginAction()
+        {
+            return "SignIn";
+        }
+
+        public string GetRedirectToLoginController()
+        {
+            return "Account";
+        }
+
+        public string GetUserId()
+        {
+            return context.HttpContext.User.Claims.FirstOrDefault(x => x.Type == "http://schemas.microsoft.com/identity/claims/objectidentifier").Value;
+        }
+
+        public UserRole GetUserRole()
+        {
+            return tmpRole;
+        }
+
+        public bool IsAuthenticated()
+        {
+            return context.HttpContext.User != null;
+        }
+    }
+}
