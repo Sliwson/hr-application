06abd1a1bf74cc22b6d8aee5fb403407259333c3
Mateusz Śliwakowski <01130631@pw.edu.pl>
2019-12-29 16:38:52 +0100

    Add version field to models #122

---
+++ hr-application/Migrations/20191229150919_Version-Field.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace hr_application.Migrations
+{
+    public partial class VersionField : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "Version",
+                table: "JobOffers",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "Version",
+                table: "Applications",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Version",
+                table: "JobOffers");
+
+            migrationBuilder.DropColumn(
+                name: "Version",
+                table: "Applications");
+        }
+    }
+}
