c106bb72fe5c8fbef39fec39bcf855b7fe6f537f
Mateusz Śliwakowski <01130631@pw.edu.pl>
2020-01-03 19:02:41 +0100

    Add migration with users table #68

---
+++ hr-application/Migrations/20200102190621_AddUsersTable.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace hr_application.Migrations
+{
+    public partial class AddUsersTable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Users",
+                columns: table => new
+                {
+                    Identifier = table.Column<string>(nullable: false),
+                    Role = table.Column<int>(nullable: false),
+                    Email = table.Column<string>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Users", x => x.Identifier);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Users");
+        }
+    }
+}
