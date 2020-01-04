86a61e53b8140057ad1ee128bbf0267dbd65c994
Mateusz Śliwakowski <01130631@pw.edu.pl>
2020-01-04 10:33:31 +0100

    Add user view model #68

---
+++ hr-application/ViewModels/UserViewModel.cs
@@ -0,0 +1,15 @@
+﻿using hr_application.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace hr_application.ViewModels
+{
+    public class UserViewModel
+    {
+        public string Identifier { get; set; }
+        public  UserRole Role { get; set; }
+        public string Email { get; set; }
+    }
+}
