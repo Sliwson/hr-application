0b474d848877adb73b34d73ba1a00489c4bcda44
Mateusz Śliwakowski <01130631@pw.edu.pl>
2020-01-05 12:32:05 +0100

    Improve db context configuration #75

--- hr-application/Models/HrContext.cs
+++ hr-application/Models/HrContext.cs
@@ -19,11 +19,45 @@ namespace hr_application
 
         protected override void OnModelCreating(ModelBuilder builder)
         {
+            //user
             builder.Entity<User>(entity =>
             {
                 entity.HasKey(s => s.Identifier);
+
                 entity.Property(s => s.Email).IsRequired();
                 entity.Property(s => s.Role).IsRequired();
+            });
+
+            //job offer
+            builder.Entity<JobOffer>(entity =>
+            {
+                entity.HasKey(j => j.Id);
+                entity.HasOne(j => j.User).WithMany(u => u.JobOffers).HasForeignKey(j => j.UserId);
+
+                entity.Property(j => j.Description).IsRequired();
+                entity.Property(j => j.ExpirationDate).IsRequired();
+                entity.Property(j => j.JobTitle).IsRequired();
+                entity.Property(j => j.Location).IsRequired();
+                entity.Property(j => j.MaximumSalary).IsRequired();
+                entity.Property(j => j.MinimumSalary).IsRequired();
+                entity.Property(j => j.UserId).IsRequired();
+                entity.Property(j => j.Version).IsRequired();
+            });
+
+            //application
+            builder.Entity<Application>(entity =>
+            {
+                entity.HasKey(a => a.Id);
+                entity.HasOne(a => a.User).WithMany(u => u.Applications).HasForeignKey(a => a.UserId);
+                entity.HasOne(a => a.RelatedOffer).WithMany(j => j.Applications).HasForeignKey(j => j.RelatedOfferId);
+
+                entity.Property(a => a.CvGuid).IsRequired();
+                entity.Property(a => a.Email).IsRequired();
+                entity.Property(a => a.FirstName).IsRequired();
+                entity.Property(a => a.LastName).IsRequired();
+                entity.Property(a => a.PhoneNumber).IsRequired();
+                entity.Property(a => a.State).IsRequired();
+                entity.Property(a => a.Version).IsRequired();
             });
         }
     }
