c065c254e3ce228a10798c95664238c1e0a985b7
Mateusz Śliwakowski <01130631@pw.edu.pl>
2020-01-06 16:14:00 +0100

    Update index view to load job offers via ajax #72

--- hr-application/wwwroot/js/site.js
+++ hr-application/wwwroot/js/site.js
@@ -1,4 +1,34 @@
-﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
+﻿function fetchData() {
+    var $loading = "<div class='loading'>Please wait...</div>";
+    console.log("loading");
+    $('#jobOffers').prepend($loading);
+    $.ajax({
+        url: '/api/JobOfferApi',
+        type: 'GET',
+        dataType: 'json',
+        success: function (data) {
+            var $table = $('<table/>').addClass('table borderless');
+            var $header = $('<thead/>').html('<tr><th>Job title</th><th>Salary</th><th>Location</th><th></th></tr>');
+            $table.append($header);
+            $.each(data, function (i, offer) {
+                var $row = $('<tr/>');
+                $row.append($('<td/>').html(offer.jobTitle));
+                $row.append($('<td/>').html(offer.salary));
+                $row.append($('<td/>').html(offer.location));
+                $button = $('<a href="/JobOffer/Details/' + offer.id + '">Details</a>').addClass('btn btn-primary');
+                $row.append($('<td/>').html($button));
+                $table.append($row);
+            });
 
-// Write your JavaScript code.
+            var $footer = $('<tr/>');
+            $table.append($footer);
+
+            $('#jobOffers').html($table);
+        },
+        error: function () {
+            alert('Error! Please try again.');
+        }
+    }).done(function () {
+        $('#jobOffers').remove('#loading');
+    });
+}
